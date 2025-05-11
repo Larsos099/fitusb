8baf6e86d9689a3acfe1d5ef1c40f6d422dcb4cf
Lars <larsmail99@gmx.de>
2025-05-11 02:15:32 +0200

    implemented arguments

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,29 @@ int main(int argc, char* argv[]){
         vb = true;
         v = YES;
     }
+    if(findInVec(argl, s("-gos"))){
+        getos = true;
+    }
+    if(findInVec(argl, s("-ld"))){
+        listdev = true;
+    }
+    if(getos){
+        os ro = getOS();
+        switch(ro){
+            case 0: {std::cout << "UNIX/UNIX-LIKE" << std::endl;} break;
+            case 1: {std::cout << "WINDOWS" << std::endl;} break;
+            case 2: {std::cout << "MACOS" << std::endl;} break;
+            case 3: {std::cout << "UNSUPPORTED" << std::endl;} break;
+        };
+    }
     s out{};
     int r{};
     Process proc(out, r);
     os osys = getOS();
+    if(listdev){
+        v = YES;
+        proc.Exec(osys, v, "lsblk");
+    }
     if((v)){
         proc.Exec(osys, v, "tree");
         std::cout << out << std::endl;
